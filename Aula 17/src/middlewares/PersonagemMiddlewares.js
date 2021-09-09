d85d1b5492d19ec776d0ef27badb6f7c189c0e74
wandersonDeve <Wanderlavo@outlook.com>
2021-09-08 21:52:38 -0300

    middlewares

---
+++ Aula 17/src/middlewares/PersonagemMiddlewares.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Personagem = require("../models/Personagem");
+
+const validaId = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).send({ error: "Id invalido" });
+    return;
+  }
+  next();
+};
+
+try {
+  const personagem = await Personagem.findById(id);
+  if (!personagem) {
+    return res.status(404).send({ msg: "Personagem não encontrado" });
+  }
+  res.personagem = personagem;
+} catch (err) {
+  return res.status(500).send({ error: err });
+}
+
+module.exports = { validaId };
