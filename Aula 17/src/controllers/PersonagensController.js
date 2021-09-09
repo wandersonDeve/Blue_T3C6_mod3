69d9caf04ce09bb24f212d44602a4b53f95d578b
wandersonDeve <Wanderlavo@outlook.com>
2021-09-08 21:02:55 -0300

    corrigindo PersonagemControllers

---
+++ Aula 17/src/controllers/PersonagensController.js
@@ -0,0 +1,10 @@
+const Personagem = require("../models/Personagem")
+
+const getAll = async (req, res) => {
+    try{
+        const personagens = await Personagem.find()
+        return res.send({ personagens })
+    }catch(err){
+        res.status(500).send({ error:err })
+    }
+}
